fcb32bd2ea012ce2fd2f79475062ed21a3df700a
Julie Shapovalenko <jools.shapo@gmail.com>
2023-03-30 01:31:13 +0300

    add src files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './sass/index.scss';
+
+import './js/refs';
+import './js/search-input';
+import './js/api-fetch';
+import './js/loader-spinner';
+import './js/markup-cards';
+import './js/modal-movie';
+import './js/pagination';
+import './js/smooth-scroll';
+import './js/notification';
+import './js/queue-local-storage';
+import './js/watched-local-storage';
+import './js/modal-team';
---
+++ src/js/api-fetch.js
@@ -0,0 +1,4 @@
+
+
+const API_KEY = '169863a84bc27c731fc45c45dd4a4a7e';
+
