cec2c06cc0eb229871536d37d44be19304bb6e33
stark1269 <stark1269@gmail.com>
2023-03-30 15:45:27 +0300

    add modal html

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,1 +1,31 @@
-import { BasicLightBox } from "basiclightbox";
+const btn = document.querySelector('.btn');
+
+// 
+
+btn.addEventListener('click', onClickMovie);
+
+function onClickMovie() {
+  modal.classList.remove('visually-hidden');
+  window.addEventListener('keydown', onCloseModal);
+};
+
+// 
+
+const modal = document.querySelector('.js-modal');
+
+function modalClose() {
+  modal.classList.add('visually-hidden');
+  window.removeEventListener('keydown', onCloseModal);
+};
+
+modal.addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    modalClose();
+  };
+});
+
+function onCloseModal(e) {
+  if (e.code !== "Escape") {
+    return
+  } modalClose();
+};
