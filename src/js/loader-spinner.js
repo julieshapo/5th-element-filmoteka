039efb771a956a78825b30324fffe2835c0741d8
stark1269 <stark1269@gmail.com>
2023-03-30 19:06:18 +0300

    add modal movie style and markup

--- src/js/loader-spinner.js
+++ src/js/loader-spinner.js
@@ -1,8 +1,12 @@
 const spinner = document.querySelector('.js-spinner');
 
+// Функция показывает спинер
+
 function showSpinner() {
   spinner.classList.remove('visually-hidden');
 };
+
+// Функция прячет спинер
 
 function hideSpinner() {
   spinner.classList.add('visually-hidden');
