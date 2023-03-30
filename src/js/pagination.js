5bf11f70739681ba25beee861ed43fe6e8986273
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-03-30 13:09:24 +0300

    add pagination + refs for pag + image

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,22 @@
-import { Pagination } from "tui-pagination";
+import { Pagination } from 'tui-pagination';
+import refs from './refs';
+
+const TUI_PAGES_VISIBLE = 5;
+
+export function createPagination(totalItems, visiblePages) {
+  const options = {
+    itemsPerPage: 9,
+    totalItems: totalItems,
+    visiblePages: visiblePages < 5 ? visiblePages : TUI_PAGES_VISIBLE,
+  };
+
+  const pagination = new Pagination(refs.pagination, options);
+
+  if (visiblePages > 1) {
+    refs.pagination.style.display = 'block';
+  } else {
+    refs.pagination.style.display = 'none';
+  }
+
+  return pagination;
+}
