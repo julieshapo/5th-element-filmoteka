dcb32088bab48be5cc53062a5e627ebfa93c230f
Julie Shapovalenko <jools.shapo@gmail.com>
2023-03-31 19:48:54 +0300

    search input js

--- src/js/search-input.js
+++ src/js/search-input.js
@@ -1,5 +1,6 @@
 import debounce from 'lodash.debounce';
 import axios from 'axios';
+import {getMoviesByName} from './api-fetch'
 
 const API_KEY = '169863a84bc27c731fc45c45dd4a4a7e';
 
@@ -17,20 +18,23 @@ function onFormSubmit(event) {
   }
 }
 
-async function getMoviesByName(name) {
-  try {
-    const params = {
-      api_key: API_KEY,
-      query: name,
-      page: 1,
-      language: 'en-US',
-    };
-    const response = await axios.get(
-      'https://api.themoviedb.org/3/search/movie',
-      { params }
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+
+
+
+// async function getMoviesByName(name) {
+//   try {
+//     const params = {
+//       api_key: API_KEY,
+//       query: name,
+//       page: 1,
+//       language: 'en-US',
+//     };
+//     const response = await axios.get(
+//       'https://api.themoviedb.org/3/search/movie',
+//       { params }
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
