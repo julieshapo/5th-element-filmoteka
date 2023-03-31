fd976c382333f66333029656f700cc8973901f55
mentallaboratories <mentallaboratories@gmail.com>
2023-03-31 18:55:49 +0300

    Update queue-local-storage.js

--- src/js/queue-local-storage.js
+++ src/js/queue-local-storage.js
@@ -1,5 +1,11 @@
 import { setQueueLS } from "./local-storage";
 
-const queueBtn = document.querySelector('.js-queue');
-queueBtn.addEventListener('click', ()=>console.log('click'))
-setQueueLS();
+const queueBtn = document.getElementById('#js-queue');
+
+queueBtn.addEventListener('click', displayQueue)
+
+function displayQueue(e){
+
+setQueueLS([{name:'dd',value:'dddd'}]);
+
+}
