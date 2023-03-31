af65c9f488029862d97712c1cd3e478b35a648b3
Kateryna Verveda <katushka211@gmail.com>
2023-03-31 16:07:43 +0200

    add fetch

--- src/js/api-fetch.js
+++ src/js/api-fetch.js
@@ -1,61 +1,113 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = '169863a84bc27c731fc45c45dd4a4a7e';
 
-const MAIN_URL = 'https://api.themoviedb.org/3';
-
-
-export async function getTrending(page = 1) {
-  const url = `${MAIN_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.log(error));
+//Запит фільму за ключовим словом:
+async function getMoviesByName(name) {
+  try {
+    const params = {
+      api_key: API_KEY,
+      query: name,
+      page: 1,
+      language: 'en-US',
+    };
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/search/movie',
+      { params }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getByKeyword(query, page) {
-  const url = `${MAIN_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.log(error));
+//Запит на список найпопулярніших фільмів
+async function getMoviesTrending(page) {
+  try {
+    const params = {
+      api_key: API_KEY,
+      language: 'en-US',
+    };
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/week?page=${page}`,
+      // `https://api.themoviedb.org/3/trending/all/week?page=${page}`,
+      { params }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getInfoMovie(movie_id) {
-  const url = `${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => {});
+//Запит повної інформації про кінофільм
+async function getMovieFullInfo(movie_id) {
+  try {
+    const params = {
+      api_key: API_KEY,
+      language: 'en-US',
+    };
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movie_id}?`,
+      { params }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getVideos(movie_id) {
-  const url = `${MAIN_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data.results;
-    })
-    .catch(error => {});
-}
+// const MAIN_URL = 'https://api.themoviedb.org/3';
 
-export async function getArrayofMovies(array) {
-  const arrayOfMovies = array.map(async movie_id => {
-    return await axios
-      .get(`${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => console.log(error));
-  });
+// export async function getTrending(page = 1) {
+//   const url = `${MAIN_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`;
+//   return await axios
+//     .get(url)
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => console.log(error));
+// }
 
-  const resultData = await Promise.all(arrayOfMovies);
-  return resultData;
-}
+// export async function getByKeyword(query, page) {
+//   const url = `${MAIN_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}`;
+//   return await axios
+//     .get(url)
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => console.log(error));
+// }
 
+// export async function getInfoMovie(movie_id) {
+//   const url = `${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
+//   return await axios
+//     .get(url)
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => {});
+// }
+
+// export async function getVideos(movie_id) {
+//   const url = `${MAIN_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`;
+//   return await axios
+//     .get(url)
+//     .then(response => {
+//       return response.data.results;
+//     })
+//     .catch(error => {});
+// }
+
+// export async function getArrayofMovies(array) {
+//   const arrayOfMovies = array.map(async movie_id => {
+//     return await axios
+//       .get(`${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
+//       .then(response => {
+//         return response.data;
+//       })
+//       .catch(error => console.log(error));
+//   });
+
+//   const resultData = await Promise.all(arrayOfMovies);
+//   return resultData;
+// }
