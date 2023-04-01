07e73b7ef46450e3428dcb8ff60c21567b358095
Denys-Kondratenko <dk132652@gmail.com>
2023-04-01 22:12:20 +0300

    update

--- src/js/markup-cards.js
+++ src/js/markup-cards.js
@@ -1,6 +1,7 @@
 import { getMoviesTrending } from './api-fetch';
 import { genresFormat } from './geners';
 import { refs } from './refs';
+import { genresArray } from './watched-local-storage';
 
 TrendingMovie();
 
@@ -9,13 +10,13 @@ TrendingMovie();
 export function createMarkupOneCard(array) {
   return array
     .map(item => {
-      const geners = genresFormat(item.genre_ids).join(', ');
+      const geners = genresFormat(item.genre_ids || genresArray()).join(', ');
       const date = item.release_date ?? item.first_air_date ?? null;
       const year = date ? date.slice(0, 4) : 'Unknown year';
 
       const poster = item.poster_path
-      ? `https://image.tmdb.org/t/p/w500/${item.poster_path}`
-      : 'https://github.com/julieshapo/5th-element-filmoteka/blob/main/src/images/no-photo/no-photo.jpg?raw=true';
+        ? `https://image.tmdb.org/t/p/w500/${item.poster_path}`
+        : 'https://github.com/julieshapo/5th-element-filmoteka/blob/main/src/images/no-photo/no-photo.jpg?raw=true';
 
       return `
     <li data-id=${item.id} class="film-item">
@@ -34,16 +35,20 @@ export function createMarkupOneCard(array) {
     `;
     })
     .join('');
-};
+}
 
-// Функция которая ожидает ответа от апи и вставляет разметку в галерею фильмов 
+// Функция которая ожидает ответа от апи и вставляет разметку в галерею фильмов
 
 async function TrendingMovie() {
   try {
     const { results } = await getMoviesTrending();
 
+    if (!refs.filmGallery) {
+      return;
+    }
+
     refs.filmGallery.innerHTML = createMarkupOneCard(results);
   } catch (error) {
     console.log(error.message);
   }
-};
+}
