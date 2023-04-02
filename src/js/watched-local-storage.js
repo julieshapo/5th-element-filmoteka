e386cfdd94a31cdb5ab96c1c2fe0edf2b57db050
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-04-03 00:26:33 +0300

    add local pagination

--- src/js/watched-local-storage.js
+++ src/js/watched-local-storage.js
@@ -5,11 +5,18 @@ import { createMarkupOneCard } from './markup-cards';
 import { onClickMovie, modalClose } from './modal-movie';
 import { refs } from './refs';
 import { btnQueue } from './queue-local-storage';
+import { createPagination } from './pagination';
 
 export const btnWatched = document.querySelector('.js-watched');
 const libraryList = document.querySelector('.js-library_gallery');
 const imgWatchedPlug = document.querySelector('.no-watched');
 const imgQueuePlug = document.querySelector('.no-queue');
+
+const ITEMS_PER_PAGES = 9;
+
+let firstFunctionRun = 0;
+let startElements = 0;
+let endElements = ITEMS_PER_PAGES;
 
 if (!libraryList) {
   return;
@@ -42,7 +49,20 @@ export function markupWatched() {
   }
   imgQueuePlug.style.display = 'none';
 
-  libraryList.innerHTML = createMarkupOneCard(watched);
+  // Пагінатор
+  const totalElements = watched.length;
+  const filtredWatched = watched.slice(startElements, endElements);
+  if (firstFunctionRun === 0) {
+    createPagination(totalElements, 3, 0, ITEMS_PER_PAGES);
+  }
+  firstFunctionRun = 1;
+
+  libraryList.innerHTML = createMarkupOneCard(filtredWatched);
+  startElements += ITEMS_PER_PAGES;
+  endElements += ITEMS_PER_PAGES;
+  ////
+  console.log(startElements);
+  console.log(endElements);
 }
 
 document.addEventListener('DOMContentLoaded', markupWatched);
