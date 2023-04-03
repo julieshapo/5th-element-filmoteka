27d9a6996281b05b2eaac0c40d75982fdced8c06
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-04-03 19:48:16 +0300

    add button top

---
+++ src/js/go-top.js
@@ -0,0 +1,21 @@
+const goTopBtn = document.querySelector('.go-top');
+
+goTopBtn.addEventListener('click', goTop);
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll() {
+  const offset = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (offset > coords) {
+    goTopBtn.classList.add('go-top--show');
+  } else {
+    goTopBtn.classList.remove('go-top--show');
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -150);
+    setTimeout(goTop, 0);
+  }
+}
